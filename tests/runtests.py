3da0b91a5ce1f74a43a81b2fff5713e944e64045
idle sign <idlesign@yandex.ru>
2015-10-23 19:26:00 +0600

    runtests updated.

--- tests/runtests.py
+++ tests/runtests.py
@@ -1,8 +1,13 @@
+import sys
 import os
 import unittest
 
 
-tests_loader = unittest.TestLoader()
-test_suit = tests_loader.discover(os.path.abspath(os.path.dirname(__file__)))
-unittest.TextTestRunner().run(test_suit)
+DIR_CURRENT = os.path.abspath(os.path.dirname(__file__))
+DIR_PARENT = os.path.dirname(DIR_CURRENT)
 
+sys.path.insert(0, DIR_PARENT)
+
+tests_loader = unittest.TestLoader()
+test_suit = tests_loader.discover(DIR_CURRENT)
+unittest.TextTestRunner().run(test_suit)
