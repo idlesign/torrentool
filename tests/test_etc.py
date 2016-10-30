ffaebbfd145d43463e3480896e3904b4058c57b9
idle sign <idlesign@yandex.ru>
2016-10-30 13:45:26 +0700

    Added humanize_filesize().

--- tests/test_etc.py
+++ tests/test_etc.py
@@ -1,6 +1,12 @@
 # -*- encoding: utf-8 -*-
-from torrentool.utils import get_app_version
+from torrentool.utils import get_app_version, humanize_filesize
 
 
 def test_get_app_version():
     assert 'torrentool' in get_app_version()
+
+
+def test_filesize():
+    assert humanize_filesize(0) == '0 B'
+    assert humanize_filesize(1024) == '1.0 KB'
+    assert humanize_filesize(1024 * 1024) == '1.0 MB'
--- torrentool/utils.py
+++ torrentool/utils.py
@@ -1,3 +1,5 @@
+import math
+
 
 def get_app_version():
     """Returns full version string including application name
@@ -6,3 +8,20 @@ def get_app_version():
     """
     from torrentool import VERSION
     return 'torrentool/%s' % '.'.join(map(str, VERSION))
+
+
+def humanize_filesize(bytes_size):
+    """Returns human readable filesize.
+
+    :param int bytes_size:
+    :rtype: str
+    """
+    if not bytes_size:
+        return '0 B'
+    
+    names = ('B', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB')
+
+    name_idx = int(math.floor(math.log(bytes_size, 1024)))
+    size = round(bytes_size / math.pow(1024, name_idx), 2)
+
+    return '%s %s' % (size, names[name_idx])
